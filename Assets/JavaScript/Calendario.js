45175f9c7a7336677952c2ad9e5c2d406b1c43a0
Crescencio Perez <crescencio.ps.code@gmail.com>
2022-07-15 23:52:28 -0500

    Co-authored-by: Paola Penagos <pioPen@users.noreply.github.com> Co-authored-by: MaríaJosé  <MariaJoseDominguezCosta@users.noreply.github.com> visualizacion de la tabla calendario junto al funcinamiento de su busqueda

--- Assets/JavaScript/Calendario.js
+++ Assets/JavaScript/Calendario.js
@@ -1,9 +1,12 @@
 const conexion = require('../conectar.js');
+const btnFecha = document.querySelector('#btnBuscarFecha');
+const {default: Swal} = require('sweetalert2');
+
 
 const ConsultarCalendario = () => {
     //var cadena;
-    $query = 'select * FROM calendario;'; // instruccion SQL
-    let tablaDiccionario = document.getElementById("tableConsultar");
+    $query = 'Select cliente.nombreComCliente, cliente.rfc, calendario.fechaDeclaracion from cliente join calendario on calendario.cliente_id_cliente=cliente.id_cliente'; // instruccion SQL
+    let tablaCalendario = document.getElementById("tableConsultar");
     conexion.query($query, function (err, rows) {
         if (err) {
             console.log("error en el query");
@@ -13,17 +16,69 @@ const ConsultarCalendario = () => {
             //lo que se extrae de la BD, queda guardado en ROWS que se vuelve lista de objetos
             var long = rows.length; // se obtiene el tamano de la lista
             for (i = 0; i < long; i++) {
-                var newRowDiccionarnio = tablaDiccionario.insertRow(-1);
-                var celdaPalabra = newRowDiccionarnio.insertCell(0);
-                var celdaConcepto = newRowDiccionarnio.insertCell(1);
-                var textoPalabra = document.createTextNode(rows[i].cliente);
-                var textConcepto = document.createTextNode(rows[i].fechaDeclaracion);
+                var newRowCalendario = tablaCalendario.insertRow(-1);
+                var celdaNombre = newRowCalendario.insertCell(0);
+                var celdaRfc = newRowCalendario.insertCell(1);
+                var celdaFecha = newRowCalendario.insertCell(2);
 
-                celdaPalabra.appendChild(textoPalabra);
-                celdaConcepto.appendChild(textConcepto);
+                var textoNombre = document.createTextNode(rows[i].nombreComCliente);
+                var textoRfc = document.createTextNode(rows[i].rfc);
+                var textoFecha = document.createTextNode(rows[i].fechaDeclaracion);
+
+                celdaNombre.appendChild(textoNombre);
+                celdaRfc.appendChild(textoRfc);
+                celdaFecha.appendChild(textoFecha);
             }
             //alert(cadena)
         }
     })
 }
 ConsultarCalendario();
+//Boton buscar fecha
+btnFecha.addEventListener('click', () => {
+    let nombre = document.getElementById('nombreSearch').value;
+    console.log(nombre);
+    let query = `Select cliente.nombreComCliente, cliente.rfc, calendario.fechaDeclaracion from cliente join calendario on calendario.cliente_id_cliente=cliente.id_cliente where nombreComCliente='${nombre}'`;
+    // instruccion SQL
+    let tablaCalendario = document.getElementById("tableConsultar");
+    tablaCalendario.innerHTML = '';
+    conexion.query(query, function (err, rows) {
+        if (err) {
+            console.log("error en el query");
+            console.log(err);
+            return;
+        } else if (rows.length == 0) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: 'error',
+                title: 'Usuario no encontrado'
+
+            })
+        } else { //! resultado en pantalla
+            //*lo que se extrae de la BD, queda guardado en ROWS que se vuelve lista de objetos
+            var newRowCalendario = tablaCalendario.insertRow(-1);
+            var celdaNombre = newRowCalendario.insertCell(0);
+            var celdaRfc = newRowCalendario.insertCell(1);
+            var celdaFecha = newRowCalendario.insertCell(2);
+
+            var textoNombre = document.createTextNode(rows[0].nombreComCliente);
+            var textoRfc = document.createTextNode(rows[0].rfc);
+            var textoFecha = document.createTextNode(rows[0].fechaDeclaracion);
+
+            celdaNombre.appendChild(textoNombre);
+            celdaRfc.appendChild(textoRfc);
+            celdaFecha.appendChild(textoFecha);
+        }
+    })
+});
