52e4c7f6f080b711a8ee8383e1dedf672d0bf007
Crescencio Perez <crescencio.ps.code@gmail.com>
2022-07-24 01:34:17 -0500

    constante no necesaria

--- Assets/JavaScript/Carpeta.js
+++ Assets/JavaScript/Carpeta.js
@@ -1,7 +1,6 @@
 const conexion = require('../conectar.js'); //CONEXION A BASE DE DATOS
 const btnBuscar = document.querySelector('#BuscarBoton'); //CREACION DE VARIABLE PARA BORON BUSCAR EN HTML
 const {default: Swal} = require('sweetalert2'); //LIBRERIA POPPOP´S
-const btnActualizar = document.querySelector('#Documentos');
 
 /*FUNCION PRA VER LAS CARPETAS DE LOS CLIENTES */
 const ConsultarCarpetas = () => { //* listo
