6c260ecab3893999f457cf77f2d980b9f5408baa
Crescencio Perez <crescencio.ps.code@gmail.com>
2022-07-24 01:28:51 -0500

    avance 2 de implementacion de modificacion de documentos

--- Assets/JavaScript/ModificarDocumentos.js
+++ Assets/JavaScript/ModificarDocumentos.js
@@ -9,6 +9,8 @@ btnActualizar.addEventListener('click', () => {
 
     // obtenemos el nombre condicional
     let nombre = document.getElementById('txtUsuario').value;
+    let documentos = document.getElementById('Documentos').value;
+
     console.log(nombre);
     // obtenemos desde la base de datos la informacion poreestablecida
     $idCliente = `SELECT id_cliente FROM cliente WHERE nombreComCliente='${nombre}'`;
@@ -33,8 +35,35 @@ btnActualizar.addEventListener('click', () => {
                         document.getElementById("Documentos").value = descDocumentos;
                         console.log(descDocumentos);
                         // se actualiza la descripcion y mandamos a la BSD la nueva informacion
+                        $actualizarDocs = `update carpeta set descDocumentos= '${documentos} where id_carpeta=${valorIdCliente}`;
+                        conexion.query($actualizarDocs, function (err) {
+                            if (err) {
+                                console.log("error en el query");
+                                console.log(err);
+                                return;
+                            } else {
+                                const Toast = Swal.mixin({ //INSTRUCCION DEL POPPOP
+                                    toast: true,
+                                    position: 'top-end',
+                                    showConfirmButton: false,
+                                    timer: 2000,
+                                    timerProgressBar: true,
+                                    didOpen: (toast) => {
+                                        toast.addEventListener('mouseenter', Swal.stopTimer)
+                                        toast.addEventListener('mouseleave', Swal.resumeTimer)
+                                    }
+                                })
 
-            $actualizarDocs = `update carpeta set descDocumentos= '${variableDatosAnteriores + ', '+variableDatosNuevos} where id_carpeta=${valorIdCliente}`;
+                                Toast.fire({
+                                    icon: 'success',
+                                    title: 'Declaracaion de documentos presentado han sido actualizados'
+
+                                })
+                                setTimeout(() => { //REDIRECCION EN UN LAPSO DE TIEMPO
+                                    window.location.href = "./Carpetas.html";
+                                }, 2000);
+                            }
+                        })
                     }
                 })
             }
