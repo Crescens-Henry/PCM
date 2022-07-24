e944befd64c675e024f2afb67ce43a4f63300b79
Crescencio Perez <crescencio.ps.code@gmail.com>
2022-07-24 11:31:04 -0500

    nueva funcion de actualizar/modificar infromacion de declaracion de documentos

--- Assets/JavaScript/ModificarDocumentos.js
+++ Assets/JavaScript/ModificarDocumentos.js
@@ -1,5 +1,5 @@
 const conexion = require('../conectar.js'); //CONEXION A BASE DE DATOS
-const btnActualizar = document.querySelector('#Documentos');
+const btnActualizar = document.querySelector('#btnDescInformacion');
 const {
     default: Swal
 } = require('sweetalert2'); //LIBRERIA POPPOP´S
@@ -34,37 +34,7 @@ btnActualizar.addEventListener('click', () => {
                         documentos = rows[0].descDocumentos;
                         document.getElementById("Documentos").value = documentos;
                         console.log(documentos);
-                        // se actualiza la descripcion y mandamos a la BSD la nueva informacion
-                        $actualizarDocs = `update carpeta set descDocumentos= '${documentos}' where cliente_id_cliente='${valorIdCliente}'`;
-                        conexion.query($actualizarDocs, function (err) {
-                            if (err) {
-                                console.log("error en el query");
-                                console.log(err);
-                                return;
-                            } else {
-                                const Toast = Swal.mixin({ //INSTRUCCION DEL POPPOP
-                                    toast: true,
-                                    position: 'top-end',
-                                    showConfirmButton: false,
-                                    timer: 2000,
-                                    timerProgressBar: true,
-                                    didOpen: (toast) => {
-                                        toast.addEventListener('mouseenter', Swal.stopTimer)
-                                        toast.addEventListener('mouseleave', Swal.resumeTimer)
-                                    }
-                                })
-
-                                Toast.fire({
-                                    icon: 'success',
-                                    title: 'Declaracaion de documentos presentado han sido actualizados'
-
-                                })
-                                setTimeout(() => { //REDIRECCION EN UN LAPSO DE TIEMPO
-                                    window.location.href = "./Carpetas.html";
-                                }, 2000);
-                            }
-                        })
-
+                        AyudanteActualizarInfo(documentos, valorIdCliente);
                     }
                 })
             }
@@ -73,3 +43,48 @@ btnActualizar.addEventListener('click', () => {
     })
 
 });
+
+function AyudanteActualizarInfo(documentos, valorIdCliente) {
+    this.tempDocumentos = documentos;
+
+    this.valorIdCliente = valorIdCliente;
+
+}
+
+function ActualizarInformacion() {
+    let documentos = document.getElementById('Documentos').value;
+
+    // se actualiza la descripcion y mandamos a la BSD la nueva informacion
+    console.log("valor desde ayudante para documentos es = " + tempDocumentos);
+    console.log("valor desde ayudante para valorIdCliente es = " + valorIdCliente);
+
+    $actualizarDocs = `update carpeta set descDocumentos= '${documentos}' where cliente_id_cliente='${valorIdCliente}'`;
+    conexion.query($actualizarDocs, function (err) {
+        if (err) {
+            console.log("error en el query");
+            console.log(err);
+            return;
+        } else {
+            const Toast = Swal.mixin({ //INSTRUCCION DEL POPPOP
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: 'success',
+                title: 'Declaracaion de documentos presentado han sido actualizados'
+
+            })
+            setTimeout(() => { //REDIRECCION EN UN LAPSO DE TIEMPO
+                window.location.href = "./Carpetas.html";
+            }, 2000);
+        }
+    })
+}
