be32940e472eaee8ca6c85e3cfa778f455ce3229
María José Domínguez Costa <majocosta4@gmail.com>
2022-07-25 02:17:28 -0500

    Update eliminar_cliente.js

--- Assets/JavaScript/eliminar_cliente.js
+++ Assets/JavaScript/eliminar_cliente.js
@@ -1,19 +1,16 @@
 const conexion = require('../conectar.js'); //CONEXION A BASE DE DATOS
 const btnBorrar = document.querySelector('#botonBorrar'); // CONSTANTE PARA BOTON
-const {
-    default: Swal
-} = require('sweetalert2'); //LIBRERIA DE POP
+const {default: Swal} = require('sweetalert2'); //LIBRERIA DE POP
 //BOTON PARA BORRAR UN CLIENTE
 btnBorrar.addEventListener('click', () => {
     let nombreCliente = document.getElementById("nombreB").value;
-    console.log(nombreCliente);
     $temp = `select id_cliente from cliente where nombreComCliente = '${nombreCliente}'`; //SE SELECCIONA ID DEL CLIENTE YA QUE ES LA LLAVE PRIMARIA, SE GUARDA EN ALGO PARECIDO AL QUERY
     conexion.query($temp, function (err, rows) {
         if (err) { //EN CASO DE ERROR
             console.log("error en el query");
             console.log(err);
             return;
-        } else if (rows.length ==0){
+        } else if (rows.length ==0){//SI EL USUARIO NO SE ENCUENTRA
             const Toast = Swal.mixin({ //POP
                 toast: true,
                 position: 'top-end',
@@ -37,9 +34,7 @@ btnBorrar.addEventListener('click', () => {
             var long = rows.length;
             for (i = 0; i < long; i++) {
                 var valorId = Number(rows[0].id_cliente); // SE EXTRAE EL VALOR DEL ID Y SE GUARDA EN UNA NUEVA VARIABLE
-
-                console.log(valorId);
-                //Instruccion SQL
+                //Instruccion SQL PARA ELIMINAR DE LA TABLA CLIENTE
                 $query = `DELETE FROM cliente where id_cliente='${valorId}'`;
                 conexion.query($query, function (err) {
                     if (err) { //EN CASO DE ERROR
